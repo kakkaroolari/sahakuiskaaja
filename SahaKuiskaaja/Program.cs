12dfb1914f70d2fbb2ff5459c2b9dbc4ba2a926d
kakkaroolari <homorunkkari2@hotmail.com>
2020-07-10 13:51:18 +0300

    Komentoriviparametrit ja notepad raportti (sirkkelin teran leveys puuttuu)

--- SahaKuiskaaja/Program.cs
+++ SahaKuiskaaja/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using CommandLine;
+using System.Diagnostics;
 
 namespace SahaKuiskaaja
 {
@@ -10,40 +12,128 @@ namespace SahaKuiskaaja
     {
         private static int min_pituus = 250;
         private static int max_pituus = 2500;
+        private static int lukumaara = 100;
+
+        public class SahaOptions
+        {
+            [Option('t', "tavara", Required = true, HelpText = "Aseta sahatavaran mitta millimetreina.")]
+            public int Tavara { get; set; }
+            [Option('m', "mitat", Required = false, HelpText = "Anna sahattavien kappaleiden mitat listattuna (space valiin).")]
+            public IEnumerable<int> Mitat { get; set; }
+        }
 
         static void Main(string[] args)
         {
-            var tavara = 4200;
+            bool testiMoodi = false;
+            IList<Sauva> sauvat = null;
+            int tavara = 0;
+            // katso parametrit
+            Parser.Default.ParseArguments<SahaOptions>(args)
+                   .WithParsed<SahaOptions>(o =>
+                   {
+                       if (null == o.Mitat)
+                       {
+                           Console.WriteLine($"Lahdetaan laskemaan TESTIMOODISSA, luoden {lukumaara} kpl kappaleita valilla {min_pituus}..{max_pituus} mm.");
+                           Console.WriteLine($"Paina 'Y' jatkaaksesi.");
+                           // luo testidataa
+                           sauvat = LuoTestidataa();
+                       }
+                       else
+                       {
+                           sauvat = new List<Sauva>(o.Mitat.Count());
+                           foreach (var mitta in o.Mitat)
+                           {
+                               sauvat.Add(new Sauva { pituus = mitta });
+                           }
+                       }
+                       tavara = o.Tavara;
+                   });
 
-            // luo testidataa
-            var sauvat = LuoTestidataa();
-            Console.WriteLine($"Sauvat sahaukseen: {string.Join(", ", sauvat)}");
+            Console.WriteLine($"Lahtee {sauvat.Count} sauvaa sahaukseen: {string.Join(", ", sauvat)}");
 
             var optimoimaton = SahaaJarjestyksessa(sauvat, tavara);
-            //Console.WriteLine($"Optimoimaton, hukka: {optimoimaton.LaskeHukka()}");
-            
-            var bruteforce = LuoIsoJoukko(sauvat, tavara);
-            //Console.WriteLine($"Hullun tuurilla löytynyt hukka: {bruteforce.LaskeHukka()}");
 
-            var puupakkaaja = BinaaripuuMenetelma(sauvat, tavara);
+            var bruteforce = LuoIsoJoukko(sauvat, tavara);
+
+            //var puupakkaaja = BinaaripuuMenetelma(sauvat, tavara);
 
             var hukka1 = optimoimaton.LaskeHukka();
             var hukka2 = bruteforce.LaskeHukka();
-            var hukka3 = puupakkaaja.LaskeHukka();
-            //Console.WriteLine(
-            //    "| Tavara: {0,10} | Optimoimaton Hukka: {1,6} | Bruteforce hukka: {2,6} | Parruja: {3,6} | Säästö: {4,10} |",
-            //    $"{tavara} mm.", hukka1, hukka2, bruteforce.Count, hukka1 - hukka2);
-            Raportti(tavara, "Optimoimaton", optimoimaton, 0);
-            Raportti(tavara, "Generatiivinen", bruteforce, hukka1);
-            Raportti(tavara, "BinaariPuu", puupakkaaja, hukka1);
+            //var hukka3 = puupakkaaja.LaskeHukka();
+            Raportti(tavara, "Optimoimaton", optimoimaton, 0, optimoimaton.Count);
+            Raportti(tavara, "Generatiivinen", bruteforce, hukka1, optimoimaton.Count);
+            //Raportti(tavara, "BinaariPuu", puupakkaaja, hukka1, optimoimaton.Count);
+
+            // valintamenettely
+            IList<Sahaus> tulos = null;
+            if (optimoimaton.Count < bruteforce.Count || (optimoimaton.Count == bruteforce.Count && hukka1 < hukka2))
+            {
+                Console.WriteLine("Optimointi ei tuottanut tulosta :(");
+                tulos = optimoimaton;
+            }
+            else
+            {
+                Console.WriteLine("Valitaan generatiivinen tulos..");
+                tulos = bruteforce;
+            }
+
+            // tulos tiedostoon
+            var fileName = GetTempFileName(".txt");
+            using (var file = new System.IO.StreamWriter(fileName))
+            {
+                file.WriteLine($"Tavara: {tavara} mm.");
+                file.WriteLine();
+                int laskuri = 0;
+                foreach(var sahausInfo in tulos)
+                {
+                    var hukka = sahausInfo.Hukka();
+                    file.WriteLine($"PARRU {++laskuri} (hukka: {hukka} mm):");
+                    var jarjestyksessa = sahausInfo.patkat;
+                    jarjestyksessa.Sort(delegate (Sauva c1, Sauva c2) { return c2.pituus.CompareTo(c1.pituus); });
+                    file.WriteLine(string.Join(", ", jarjestyksessa));
+                    file.WriteLine();
+                }
+                file.Flush();
+            }
+
+            // avaa notepadissa
+            var process = Process.Start("notepad.exe", fileName);
+            Console.WriteLine("Ohjelma odottaa Notepadin sulkemista.");
+            process.WaitForExit();
+            File.Delete(fileName);
         }
 
-        private static void Raportti(int tavara, string nimi, IList<Sahaus> parruja, int referenssiHukka)
+        internal static string GetTempFileName(string extension)
+        {
+            int attempt = 0;
+            while (true)
+            {
+                string fileName = Path.GetRandomFileName();
+                fileName = Path.ChangeExtension(fileName, extension);
+                var hakemisto = Path.GetTempPath();
+                fileName = Path.Combine(hakemisto, fileName);
+
+                try
+                {
+                    using (new FileStream(fileName, FileMode.CreateNew)) { }
+                    return fileName;
+                }
+                catch (IOException ex)
+                {
+                    if (++attempt == 10)
+                        throw new IOException($"Tiedoston luonti ei onnistu, siivoa hakemistoa {hakemisto}.", ex);
+                }
+            }
+        }
+
+        private static void Raportti(int tavara, string nimi, IList<Sahaus> parruja, int referenssiHukka, int referenssiParruja)
         {
             var hukka = parruja.LaskeHukka();
-            var saasto = referenssiHukka - hukka;
+            var kappaleErotus = referenssiParruja - parruja.Count;
+            var saasto = (referenssiHukka - hukka) + kappaleErotus*tavara;
+
             Console.WriteLine(
-                "| Menetelma: {0,15} | Tavara: {1,10} | hukka: {2,6} | Parruja: {3,6} | Säästö: {4,10} |",
+                "| Menetelma: {0,15} | Tavara: {1,10} | hukka: {2,6} | Parruja: {3,6} | Saastö: {4,10} |",
                 nimi, $"{tavara} mm.", hukka, parruja.Count, Metrit(saasto));
         }
 
@@ -59,7 +149,7 @@ namespace SahaKuiskaaja
             int pieninHukka = hullunTuuria.LaskeHukka();
             var _lock = new Object();
             // foreach (var sahausSuunnitelma in testiJoukko)
-            Parallel.For(0, 5000, i =>
+            Parallel.For(0, 25000, i =>
             {
                 var sekoitettu = new List<Sauva>(sauvat);
                 sekoitettu.Shuffle();
@@ -94,7 +184,7 @@ namespace SahaKuiskaaja
             var sahaukset = new List<Sahaus> { new Sahaus(tavaranPituus) };
             foreach(var sauva in sauvat)
             {
-                if (sauva.pituus > tavaranPituus) throw new Exception("Ei näitä voi sahata");
+                if (sauva.pituus > tavaranPituus) throw new Exception("Ei naita voi sahata");
                 bool eiMahtunut = true;
                 foreach(var sahaus in sahaukset)
                 {
@@ -128,7 +218,7 @@ namespace SahaKuiskaaja
                 for (int indeksi = 0; indeksi < kopio.Count; indeksi++)
                 {
                     var ehdokas = kopio.ElementAt(indeksi);
-                    if (ehdokas.pituus > tavaranPituus) throw new Exception("Ei näitä voi sahata");
+                    if (ehdokas.pituus > tavaranPituus) throw new Exception("Ei naita voi sahata");
                     if (ehdokas.pituus <= nykyinenSahaus.Hukka())
                     {
                         // sahataan sauva tasta
@@ -223,7 +313,7 @@ namespace SahaKuiskaaja
             _tavara = tavara;
         }
 
-        private List<Sauva> patkat
+        internal List<Sauva> patkat
         {
             get
             {
